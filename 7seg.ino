590cbb15b02f8fd90e1d7d9b38e8aba5bb027c82
Sahil <51375919+sa1123@users.noreply.github.com>
2024-07-05 21:07:26 +0530

    Add files via upload

---
+++ 7seg.ino
@@ -0,0 +1,118 @@
+int ledPin = 9;      // LED connected to digital pin 9
+
+int threePin = 3;   // potentiometer connected to analog pin 3
+int fivePin = 5;
+int sixPin = 6;
+int ninePin = 9;
+int tenPin = 10;
+int elevenPin = 11;
+
+// int val = 3.3;         // variable to store the read value
+
+void setup() {
+  pinMode(ledPin, OUTPUT);  // sets the pin as output
+  pinMode(threePin, OUTPUT);
+  pinMode(fivePin, OUTPUT);
+  pinMode(sixPin, OUTPUT);
+  pinMode(ninePin, OUTPUT);
+  pinMode(tenPin, OUTPUT);
+  pinMode(elevenPin, OUTPUT);
+  pinMode(A0, OUTPUT);
+}
+
+void loop() {
+  
+  for(int i=10; i>-1; i--){
+    delay(1000);
+    if(i == 0){
+      analogWrite(threePin, 100);
+      analogWrite(fivePin, 100);
+      analogWrite(sixPin, 100);
+      analogWrite(ninePin, 100);
+      analogWrite(tenPin, 100);
+      analogWrite(elevenPin, 100);
+      digitalWrite(A0, LOW);
+    }else if(i == 1){
+      analogWrite(threePin, 100);
+      analogWrite(fivePin, 100);
+      analogWrite(sixPin, 0);
+      analogWrite(ninePin, 0);
+      analogWrite(tenPin, 0);
+      analogWrite(elevenPin, 0);
+      digitalWrite(A0, LOW);
+    }else if(i == 2){
+      analogWrite(threePin, 100);
+      analogWrite(fivePin, 0);
+      analogWrite(sixPin, 100);
+      analogWrite(ninePin, 100);
+      analogWrite(tenPin, 100);
+      analogWrite(elevenPin, 0);
+      digitalWrite(A0, HIGH);
+    }else if(i == 3){
+      analogWrite(threePin, 100);
+      analogWrite(fivePin, 100);
+      analogWrite(sixPin, 100);
+      analogWrite(ninePin, 0);
+      analogWrite(tenPin, 100);
+      analogWrite(elevenPin, 0);
+      digitalWrite(A0, HIGH);
+    }else if(i == 4){
+      analogWrite(threePin, 100);
+      analogWrite(fivePin, 100);
+      analogWrite(sixPin, 0);
+      analogWrite(ninePin, 0);
+      analogWrite(tenPin, 0);
+      analogWrite(elevenPin, 100);
+      digitalWrite(A0, HIGH);
+    }else if(i == 5){
+      analogWrite(threePin, 0);
+      analogWrite(fivePin, 100);
+      analogWrite(sixPin, 100);
+      analogWrite(ninePin, 0);
+      analogWrite(tenPin, 100);
+      analogWrite(elevenPin, 100);
+      digitalWrite(A0, HIGH);
+    }else if(i == 6){
+      analogWrite(threePin, 0);
+      analogWrite(fivePin, 100);
+      analogWrite(sixPin, 100);
+      analogWrite(ninePin, 100);
+      analogWrite(tenPin, 100);
+      analogWrite(elevenPin, 100);
+      digitalWrite(A0, HIGH);
+    }else if(i == 7){
+      analogWrite(threePin, 100);
+      analogWrite(fivePin, 100);
+      analogWrite(sixPin, 0);
+      analogWrite(ninePin, 0);
+      analogWrite(tenPin, 100);
+      analogWrite(elevenPin, 0);
+      digitalWrite(A0, LOW);
+    }else if(i == 8){
+      analogWrite(threePin, 100);
+      analogWrite(fivePin, 100);
+      analogWrite(sixPin, 100);
+      analogWrite(ninePin, 100);
+      analogWrite(tenPin, 100);
+      analogWrite(elevenPin, 100);
+      digitalWrite(A0, HIGH);
+    }else if(i == 9){
+      analogWrite(threePin, 100);
+      analogWrite(fivePin, 100);
+      analogWrite(sixPin, 100);
+      analogWrite(ninePin, 0);
+      analogWrite(tenPin, 100);
+      analogWrite(elevenPin, 100);
+      digitalWrite(A0, HIGH);
+    }else{
+      analogWrite(threePin, 0);
+      analogWrite(fivePin, 0);
+      analogWrite(sixPin, 0);
+      analogWrite(ninePin, 0);
+      analogWrite(tenPin, 0);
+      analogWrite(elevenPin, 0);
+      digitalWrite(A0, LOW);
+    }
+  }
+   // analogRead values go from 0 to 1023, analogWrite values from 0 to 255
+}
